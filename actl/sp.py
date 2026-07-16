2b7ce44ab752618a9e30a5ffa90fc01d6801d90b
WebDevChaos987 <avery.mancini.2016@gmail.com>
2026-07-16 17:23:01 +0800

    Sp (space print).py

---
+++ actl/sp.py
@@ -0,0 +1,12 @@
+import sys
+from .wait import wait
+
+def typewriter(text, speed=100000):
+    """
+    Prints text one character at a time like a video game dialogue box.
+    """
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        wait(speed)  # Uses your custom wait loop between letters
+    print()  # Moves to a new line at the end
